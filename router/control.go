8f74bf31d14f730de7727f297fd88004d67ce970
wangrui <hary654321@126.com>
2024-06-20 16:36:25 +0800

    base64

--- router/control.go
+++ router/control.go
@@ -4,12 +4,13 @@
  * @Autor: ABing
  * @Date: 2024-06-19 11:14:31
  * @LastEditors: lhl
- * @LastEditTime: 2024-06-20 15:23:15
+ * @LastEditTime: 2024-06-20 16:12:25
  */
 
 package router
 
 import (
+	"encoding/base64"
 	"gotoexec/global"
 	"gotoexec/grpcapi"
 	"gotoexec/middlewares"
@@ -17,6 +18,7 @@ import (
 	"gotoexec/util"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -48,15 +50,34 @@ func InitControlRouter(Router *gin.RouterGroup) {
 				return
 			}
 
-			out, err := util.DecryptByAes(cmdout.Out)
+			outstring := ""
 
-			if err != nil {
-				log.Panicln(err)
+			switch cmd {
+			case "screenshot":
+				images := strings.Split(cmdout.Out, ";")
+
+				for _, j := range images {
+					if j == "" {
+						break
+					}
+					image, err := util.DecryptByAes(j)
+					if err != nil {
+						log.Fatal(err.Error())
+					}
+					outstring += (base64.StdEncoding.EncodeToString(image) + ";")
+				}
+			default:
+				out, err := util.DecryptByAes(cmdout.Out)
+
+				if err != nil {
+					log.Panicln(err)
+				}
+				outstring = string(out)
 			}
 
 			context.JSON(http.StatusOK, gin.H{
 				"code": http.StatusOK,
-				"data": string(out),
+				"data": outstring,
 				"msg":  "health",
 			})
 		})
